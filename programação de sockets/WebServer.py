6b0a092d9b0033bedcdf200b4565ddcef211ea1b
Daniel Silva <returndaniels@gmail.com>
2023-09-17 19:12:42 -0300

    Código reestruturado e adicionadas docs strings

--- programação de sockets/WebServer.py
+++ programação de sockets/WebServer.py
@@ -10,6 +10,16 @@ sessions = {}
 
 
 def verify_user(username):
+    """
+    Verifica se um usuário com o nome de usuário fornecido existe no banco de dados.
+
+    Args:
+        username (str): O nome de usuário a ser verificado.
+
+    Returns:
+        tuple or None: Uma tupla contendo as informações do usuário se encontrado,
+        ou None se o usuário não existir no banco de dados.
+    """
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
@@ -19,6 +29,17 @@ def verify_user(username):
 
 
 def send_page_response(connectionSocket, response_header, filename):
+    """
+    Envia uma resposta HTTP para o cliente, incluindo um cabeçalho e o conteúdo de um arquivo.
+
+    Args:
+        connectionSocket (socket): O socket de conexão com o cliente.
+        response_header (str): O cabeçalho da resposta HTTP.
+        filename (str): O nome do arquivo a ser enviado como conteúdo da resposta.
+
+    Returns:
+        None
+    """
     try:
         with open(filename, "rb") as file:
             outputdata = file.read()
@@ -31,7 +52,72 @@ def send_page_response(connectionSocket, response_header, filename):
     connectionSocket.send(b"\r\n")
 
 
+def handle_login(connectionSocket, message):
+    """
+    Lida com a solicitação de login do cliente, verifica as credenciais e cria uma sessão se válido.
+
+    Args:
+        connectionSocket (socket): O socket de conexão com o cliente.
+        message (str): A mensagem HTTP recebida do cliente.
+
+    Returns:
+        None
+    """
+    _, data = message.split("\r\n\r\n")
+    data = urllib.parse.parse_qs(data)
+    username = data["username"][0]
+    password = data["password"][0]
+
+    user = verify_user(username)
+    if user:
+        _, _, user_password, salt = user
+
+    if user and verify_password(password, user_password, salt):
+        session_id = str(uuid.uuid4())
+
+        sessions[session_id] = username
+
+        response_header = "HTTP/1.1 302 Found\r\nLocation: /home\r\nSet-Cookie: session_id={}\r\n\r\n".format(
+            session_id
+        )
+        connectionSocket.send(response_header.encode())
+    else:
+        response_header = "HTTP/1.1 401 Unauthorized\r\n\r\n"
+        send_page_response(
+            connectionSocket, response_header, "./static/unauthorized.html"
+        )
+
+
+def handle_logoff(connectionSocket, username):
+    """
+    Lida com a solicitação de logoff do cliente e encerra a sessão do usuário.
+
+    Args:
+        connectionSocket (socket): O socket de conexão com o cliente.
+        username (str): O nome de usuário da sessão a ser encerrada.
+
+    Returns:
+        None
+    """
+    for session_id, user in sessions.items():
+        if user == username:
+            del sessions[session_id]
+            break
+
+    response_header = "HTTP/1.1 302 Found\r\nLocation: /login\r\n\r\n"
+    connectionSocket.send(response_header.encode())
+
+
 def handle_request(connectionSocket):
+    """
+    Lida com a solicitação HTTP recebida do cliente, roteia para as funções apropriadas e envia a resposta.
+
+    Args:
+        connectionSocket (socket): O socket de conexão com o cliente.
+
+    Returns:
+        None
+    """
     request_method = None
     is_logged = False
     message = connectionSocket.recv(1024).decode()
@@ -52,37 +138,9 @@ def handle_request(connectionSocket):
         request_method, _, _, _, _ = message.split()[:5]
 
     if request_method == "POST" and "/login" in message:
-        _, data = message.split("\r\n\r\n")
-        data = urllib.parse.parse_qs(data)
-        username = data["username"][0]
-        password = data["password"][0]
-
-        user = verify_user(username)
-        if user:
-            _, _, user_password, salt = user
-
-        if user and verify_password(password, user_password, salt):
-            session_id = str(uuid.uuid4())
-
-            sessions[session_id] = username
-
-            response_header = "HTTP/1.1 302 Found\r\nLocation: /home\r\nSet-Cookie: session_id={}\r\n\r\n".format(
-                session_id
-            )
-            connectionSocket.send(response_header.encode())
-        else:
-            response_header = "HTTP/1.1 401 Unauthorized\r\n\r\n"
-            send_page_response(
-                connectionSocket, response_header, "./static/unauthorized.html"
-            )
+        handle_login(connectionSocket, message)
     elif request_method == "POST" and "/logoff" in message:
-        for session_id, user in sessions.items():
-            if user == username:
-                del sessions[session_id]
-                break
-
-        response_header = "HTTP/1.1 302 Found\r\nLocation: /login\r\n\r\n"
-        connectionSocket.send(response_header.encode())
+        handle_logoff(connectionSocket, username)
     elif len(message.split()) > 0:
         filename = message.split()[1]
         filename = "/index.html" if filename == "/" else filename
@@ -101,6 +159,12 @@ def handle_request(connectionSocket):
 
 
 def main():
+    """
+    Função principal que configura o servidor e inicia o loop de escuta por conexões.
+
+    Returns:
+        None
+    """
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverPort = 3000
     serverSocket.bind(("", serverPort))
